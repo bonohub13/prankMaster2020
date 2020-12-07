6baa10929c14cbefef080fe5f44b923e453a5c92
bonohub13 <ken.saito.0813@gmail.com>
2020-12-08 01:31:39 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Prank Master 2020
+
+## What this does:
+- Opens up random websites in a list.
+    - Note:
+        - Nothing seriously dangerous or harmful!
+        - You can add websites of your choice to the list.
+            - The list is ```database.txt``` file under scripts.
+## How to use:
+1. Install Python3.x on the PC you plan to use this prank on.
+    - from here! ```https://www.python.org/```
+    - If your ~~victim~~ (cough cough) friend's PC is running Linux or Mac OS than it is likely that it is already installed.
+2. Set this program to run on startup.
+3. Have fun!
---
+++ scripts/prankmaster.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import platform
+from random import randint
+from os import system
+import pathlib
+
+class PrankMaster:
+    def __init__(self, debug: bool=False):
+        os = platform.system()
+        self.os = "Mac" if "Darwin" in os else "Windows" if "Windows" in os else "Linux"
+        self.urls = []
+        filepath = str(pathlib.Path(__file__).parent.absolute())
+        if self.os == "Windows":
+            filepath += "\\"
+        else:
+            filepath += "/"
+        with open(f"{filepath}database.txt", "r") as data:
+            for url in data.readlines():
+                if url != "":
+                    if "\n" in url:
+                        self.urls.append(url[:-1])
+                    else:
+                        self.urls.append(url)
+        self.command = {
+            "Linux"  : f"firefox {self.urlPicker()}",
+            "Mac"    : f"open -a Safari {self.urlPicker()}",
+            "Windows": f"start msedge {self.urlPicker()}"
+        }
+
+    def urlPicker(self) -> str:
+        fibonacci_random = [0, 1]
+        while len(fibonacci_random) < 100:
+            fibonacci_random.append(fibonacci_random[-2] + fibonacci_random[-1])
+        random = fibonacci_random[randint(0, len(fibonacci_random))]*fibonacci_random[randint(0, len(fibonacci_random))]//fibonacci_random[randint(0, len(fibonacci_random))]%len(self.urls)
+        
+        return self.urls[random]
+    
+    def run(self):
+        system(self.command[self.os])
+
+if "main" in __name__:
+    print(pathlib.Path(__file__).parent.absolute())
+    prank = PrankMaster()
+    prank.run()
